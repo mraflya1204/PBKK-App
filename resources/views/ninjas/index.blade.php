130f64ff02ec4aba6132ca54c234aa808621249d
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-09-03 20:50:16 +0700

    Video 2

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network | Home</title>
+</head>
+<body>
+    <h2>Currently Available Ninjas</h2>
+    <ul>
+        <li>Hattori Hanzo</li>
+        <li>Fuma Kotaro</li>
+        <li>Kato Danzo</li>
+        <li>Momochi Sandayu</li>
+        <li>Ishikawa Goemon</li>
+    </ul>
+</body>
+</html>
