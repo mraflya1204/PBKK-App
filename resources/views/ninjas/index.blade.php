811993ea50e593927e3fb1997e3327b719687e5e
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-09-03 21:01:55 +0700

    Video 3

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -6,12 +6,36 @@
 </head>
 <body>
     <h2>Currently Available Ninjas</h2>
+    <p>{{ $greeting }}, here is a list of our top ninjas:</p>
     <ul>
-        <li>Hattori Hanzo</li>
-        <li>Fuma Kotaro</li>
-        <li>Kato Danzo</li>
-        <li>Momochi Sandayu</li>
-        <li>Ishikawa Goemon</li>
+        <li>
+            <a href="/ninjas/{{ $ninjas[0]["id"] }}">
+                {{ $ninjas[0]['name'] }} - Age: {{ $ninjas[0]['age'] }}, Specialty: {{ $ninjas[0]['specialty'] }}
+            </a>
+        </li>
+        <li>
+            <a href="/ninjas/{{ $ninjas[1]["id"] }}">
+                {{ $ninjas[1]['name'] }} - Age: {{ $ninjas[1]['age'] }}, Specialty: {{ $ninjas[1]['specialty'] }}
+            </a>
+        </li>
+        <li>
+            <a href="/ninjas/{{ $ninjas[2]["id"] }}">
+                {{ $ninjas[2]['name'] }} - Age: {{ $ninjas[2]['age'] }}, Specialty: {{ $ninjas[2]['specialty'] }}
+            </a>
+        </li>
+        <li>
+            <a href="/ninjas/{{ $ninjas[3]["id"] }}">
+                {{ $ninjas[3]['name'] }} - Age: {{ $ninjas[3]['age'] }}, Specialty: {{ $ninjas[3]['specialty'] }}
+            </a>
+        </li>
+        <li>
+            <a href="/ninjas/{{ $ninjas[4]["id"] }}">
+                {{ $ninjas[4]['name'] }} - Age: {{ $ninjas[4]['age'] }}, Specialty: {{ $ninjas[4]['specialty'] }}
+            </a>
+        </li>
+        <li>
+            And many more...
+        </li>
     </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,16 @@ Route::get('/', function () {
 });
 
 Route::get('/ninjas', function () {
-    return view('ninjas.index');
+    $ninjas = [
+        ["id" => "1", "name" => "Hattori Hanzo", "age" => 35, "specialty" => "Stealth"],
+        ["id" => "2", "name" => "Fuma Kotaro", "age" => 30, "specialty" => "Espionage"],
+        ["id" => "3", "name" => "Kato Danzo", "age" => 40, "specialty" => "Assassination"],
+        ["id" => "4", "name" => "Momochi Sandayu", "age" => 28, "specialty" => "Martial Arts"],
+        ["id" => "5", "name" => "Ishikawa Goemon", "age" => 33, "specialty" => "Thievery"],
+    ];
+    return view('ninjas.index', ['greeting' => 'Hello', 'ninjas' => $ninjas]);
+});
+
+Route::get('/ninjas/{id}', function ($id) {
+    return view('ninjas.show', ['id' => $id]);
 });
