e6339cb6b864fb64f369c304fe5baa364362f583
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-09-09 21:41:43 +0700

    Video 4 - Blade Directives

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -7,35 +7,23 @@
 <body>
     <h2>Currently Available Ninjas</h2>
     <p>{{ $greeting }}, here is a list of our top ninjas:</p>
+
+    @if($greeting == "Hello")
+        <p>Welcome back, esteemed guest!, this is from inside the if statement</p>
+    @endif
+
     <ul>
-        <li>
-            <a href="/ninjas/{{ $ninjas[0]["id"] }}">
-                {{ $ninjas[0]['name'] }} - Age: {{ $ninjas[0]['age'] }}, Specialty: {{ $ninjas[0]['specialty'] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{ $ninjas[1]["id"] }}">
-                {{ $ninjas[1]['name'] }} - Age: {{ $ninjas[1]['age'] }}, Specialty: {{ $ninjas[1]['specialty'] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{ $ninjas[2]["id"] }}">
-                {{ $ninjas[2]['name'] }} - Age: {{ $ninjas[2]['age'] }}, Specialty: {{ $ninjas[2]['specialty'] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{ $ninjas[3]["id"] }}">
-                {{ $ninjas[3]['name'] }} - Age: {{ $ninjas[3]['age'] }}, Specialty: {{ $ninjas[3]['specialty'] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{ $ninjas[4]["id"] }}">
-                {{ $ninjas[4]['name'] }} - Age: {{ $ninjas[4]['age'] }}, Specialty: {{ $ninjas[4]['specialty'] }}
-            </a>
-        </li>
+        @foreach($ninjas as $ninja)
+            <li>
+                <a href="/ninjas/{{ $ninja['id'] }}">
+                    {{ $ninja['name'] }} - Age: {{ $ninja['age'] }}, Specialty: {{ $ninja['specialty'] }}
+                </a>
+            </li>
+        @endforeach
         <li>
             And many more...
         </li>
     </ul>
+
 </body>
 </html>
