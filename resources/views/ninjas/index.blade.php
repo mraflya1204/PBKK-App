674180b43a695f21590baa2f00b4744f9dba4720
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-09-09 21:52:24 +0700

    Video 5 - Layouts and Slots

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,10 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninja Network | Home</title>
-</head>
-<body>
+<x-layout>
     <h2>Currently Available Ninjas</h2>
     <p>{{ $greeting }}, here is a list of our top ninjas:</p>
 
@@ -24,6 +18,4 @@
             And many more...
         </li>
     </ul>
-
-</body>
-</html>
+</x-layout>
