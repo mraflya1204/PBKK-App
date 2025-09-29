db631403aff115c5e80642e3a9a08b057fe3a3b6
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-09-29 21:57:29 +0700

    Opps I forgot this is not in the css app

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Ninja Details')
     <h2>Ninja ID - {{ $ninja->id }}</h2>
 
-    <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+    <div class="bg-gray-700 p-6 rounded-lg shadow-md">
         <h3 class="text-2xl font-bold mb-4">{{ $ninja->name }}</h3>
         <p class="mb-2"><strong>Age:</strong> {{ $ninja->age }}</p>
         <p class="mb-2"><strong>Skill Level:</strong> {{ $ninja->skill }}</p>
@@ -11,7 +11,7 @@
 
     <p></p>
 
-    <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+    <div class="bg-gray-700 p-6 rounded-lg shadow-md">
         <p class="mb-2"><strong>Dojo Name:</strong> {{ $ninja->dojo->name }}</p>
         <p class="mb-2"><strong>Dojo Location:</strong> {{ $ninja->dojo->location }}</p>
         <p class="mb-2"><strong>Dojo Description:</strong> {{ $ninja->dojo->description }}</p>
@@ -19,7 +19,7 @@
 
     <p></p>
 
-    <a href="{{ route('ninjas.index') }}" class="rounded px-3 py-2 bg-gray-200 hover:bg-blue-900 hover:text-white inline-block">
+    <a href="{{ route('ninjas.index') }}" class="rounded px-3 py-2 bg-gray-700 hover:bg-red-900 hover:text-white inline-block">
         Return to Ninja List
     </a>
     
@@ -28,6 +28,6 @@
     <form action="{{ route('ninjas.destroy', $ninja->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this ninja?');">
         @csrf
         @method('DELETE')
-        <button type="submit" class="rounded px-3 py-2 bg-gray-200 hover:bg-red-900 hover:text-white">Delete Ninja</button>
+        <button type="submit" class="rounded px-3 py-2 bg-gray-700 hover:bg-red-900 hover:text-white">Delete Ninja</button>
 
 </x-layout>
